0c08413fb613922a7c6a96dba2d212f1a2104715
zillani <shaikzillani@gmail.com>
2019-08-23 15:28:48 +0530

    cache clear

--- src/karel/SteepleChase.java
+++ src/karel/SteepleChase.java
@@ -1,44 +1,49 @@
 package karel;
+
 import stanford.karel.SuperKarel;
 
 public class SteepleChase extends SuperKarel {
-	public void run() {
+    public void run() {
 
-		while (frontIsClear()) {
-			putBeeper();
-			if(beepersPresent())
-			move();
-			if (frontIsBlocked()) {
-				jump();
-				turnLeft();
-			}
-		}
-	}
+        while (frontIsClear()) {
+            putBeeper();
+            if (beepersPresent())
+                move();
+            if (frontIsBlocked()) {
+                jump();
+                turnLeft();
+            }
+        }
+    }
 
-	public void jump() {
+    public static void main(String[] args) {
+        new SteepleChase().start(args);
+    }
 
-		print("inside jump");
-		turnLeft();
-		
-		while (rightIsBlocked()) {
-			if(frontIsBlocked()) {
-				System.exit(0);
-			}
-			//moveup
-			move();
-		}
-		
-		println("turn right");
-		turnRight();
-		println("move it");
-		move();
-		println("turn right again");
-		turnRight();
-		println("and again");
-		//moving down the pole
-		while (frontIsClear()) {
-			println("inside while loop");
-			move();
-		}
-	}
+    public void jump() {
+
+        print("inside jump");
+        turnLeft();
+
+        while (rightIsBlocked()) {
+            if (frontIsBlocked()) {
+                System.exit(0);
+            }
+            //moveup
+            move();
+        }
+
+        println("turn right");
+        turnRight();
+        println("move it");
+        move();
+        println("turn right again");
+        turnRight();
+        println("and again");
+        //moving down the pole
+        while (frontIsClear()) {
+            println("inside while loop");
+            move();
+        }
+    }
 }
